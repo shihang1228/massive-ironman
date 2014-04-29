7ff7583c3ed848077d5ad71ae7b0dcfad373bc73
shihang <study1228.sh@gmail.com>
2014-04-29 10:51:26 +0800

    从数据库中查询出菜品类型为凉菜的菜品放到集合中

--- src/main/java/com/order/FoodDao.java
+++ src/main/java/com/order/FoodDao.java
@@ -41,5 +41,48 @@ public class FoodDao
         }
     }
     
+    public ArrayList<FoodVo> coldFoodList()
+    {
+        DbManage dbManage = null;
+        PreparedStatement pstmt = null;
+        Connection conn = null;
+        ResultSet rs = null;
+        FoodVo food = null;
+        ArrayList<FoodVo> foodList = new ArrayList<FoodVo>();
+        
+        try
+        {
+            dbManage = dbManage.newInstance();
+            String sql = "SELECT * FROM food_list where food_type=?";
+            pstmt = dbManage.conn.prepareStatement(sql);
+            pstmt.setString(1,"凉菜");
+            rs = pstmt.executeQuery();
+            System.out.println(sql);
+            while(rs.next())
+            {
+                food = new FoodVo();
+                food.setFoodName(rs.getString("food_name"));
+                food.setFoodPrice(rs.getString("food_price"));
+                food.setFoodDescription(rs.getString("food_description"));
+                food.setFoodPicture(rs.getString("food_picture"));
+
+                foodList.add(food);
+            }
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            System.out.println("error");
+        }
+        finally
+        {
+            dbManage.close();
+        }
+        return foodList;
+        
+    }
+    
     
 }
