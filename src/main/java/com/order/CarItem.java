572ce1a4bd88e24ad9bff186294ab25d77c114af
shihang <study1228.sh@gmail.com>
2014-05-03 13:14:01 +0800

    创建用于获取和设置购物车中所要添加的物品的类

---
+++ src/main/java/com/order/CarItem.java
@@ -0,0 +1,46 @@
+package com.order;
+
+public class CarItem
+{
+    private String foodName;
+    private String foodPrice;
+    private String foodPicture;
+    private Integer foodNumber;
+    
+    public String getFoodName()
+    {
+        return foodName;
+    }
+    public void setFoodName(String foodName)
+    {
+        this.foodName = foodName;
+    }
+    
+    public String getFoodPrice()
+    {
+        return foodPrice;
+    }
+    public void setFoodPrice(String foodPrice)
+    {
+        this.foodPrice = foodPrice;
+    }
+    
+    public String getFoodPicture()
+    {
+        return foodPicture;
+    }
+    public void setFoodPicture(String foodPicture)
+    {
+        this.foodPicture = foodPicture;
+    }
+    
+    public Integer getFoodNumber()
+    {
+        return foodNumber;
+    }
+    public void setFoodNumber(Integer foodNumber)
+    {
+        this.foodNumber = foodNumber;
+    }
+    
+}
