6ca2266efa1a3a95cc2d1eb8d4f4bc3783707837
shihang <study1228.sh@gmail.com>
2014-05-05 11:34:36 +0800

    添加hotFoodList方法来从数据库中查询出菜的类型为热菜的食物

--- src/main/java/com/order/FoodDao.java
+++ src/main/java/com/order/FoodDao.java
@@ -84,6 +84,47 @@ public class FoodDao
         return foodList;
         
     }
+    public ArrayList<FoodVo> hotFoodList()
+    {
+        DbManage dbManage = null;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        FoodVo food = null;
+        ArrayList<FoodVo> foodList = new ArrayList<FoodVo>();
+        
+        try
+        {
+            dbManage = dbManage.newInstance();
+            String sql = "SELECT * FROM food_list WHERE food_type=?";
+            pstmt = dbManage.conn.prepareStatement(sql);
+            pstmt.setString(1,"热菜");
+            rs = pstmt.executeQuery();
+            System.out.println(sql);
+            while(rs.next())
+            {
+                food = new FoodVo();
+                food.setFoodId(rs.getLong("food_id"));
+                food.setFoodName(rs.getString("food_name"));
+                food.setFoodPrice(rs.getString("food_price"));
+                food.setFoodDescription(rs.getString("food_description"));
+                food.setFoodPicture(rs.getString("food_picture"));
+                foodList.add(food);               
+            }            
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            System.out.println("error");
+        }
+        finally
+        {
+            dbManage.close();
+        }
+        return foodList;
+    }
     public FoodVo getFoodById(Long pid)
     {
         DbManage dbManage = null;
